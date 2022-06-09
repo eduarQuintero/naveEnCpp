edb474cca23e5e4103bbdf4375d21dddfad770f3
eduar <eduar.curi73@outlook.com>
2022-06-08 22:17:05 -0500

    enemigos terminados

--- main.cpp
+++ main.cpp
@@ -113,14 +113,26 @@ int main() {
 	nave(posnaveX, posnaveY);
 	
 	// posicion inicial del enemigo
-	int posenemigoX = 25;
-	int posenemigoY = 100;
+	int posenemigo1X = 25;
+	int posenemigo1Y = 200;
+	int posenemigo2X = 200;
+	int posenemigo2Y = 50;
+	int posenemigo3X = 230;
+	int posenemigo3Y = 200;
+	int posenemigo4X = 500;
+	int posenemigo4Y = 120;
 	
 	//inicio del movimiento (hacia la derecha)
-	bool direcenemigo = true;
+	bool direcenemigo1 = true;
+	bool direcenemigo2 = true;
+	bool direcenemigo3 = true;
+	bool direcenemigo4 = false;
 	
 	// enemigo
-	enemigo(posenemigoX, posenemigoY);
+	enemigo(posenemigo1X, posenemigo1Y);
+	enemigo(posenemigo2X, posenemigo2Y);
+	enemigo(posenemigo3X, posenemigo3Y);
+	enemigo(posenemigo4X, posenemigo4Y);
 	
 	// dibujamos todo
 	refresca();
@@ -132,7 +144,7 @@ int main() {
 		
 		switch(t) {
 			case ARRIBA:
-				posnaveY -= (posnaveY >= 400)?10:0;
+				posnaveY -= (posnaveY >= 45)?10:0;
 				break;
 			case ABAJO:
 				posnaveY += (posnaveY <= 745)?10:0;
@@ -151,9 +163,21 @@ int main() {
 		LOS NUMEROS 515 Y 35 SE PONEN PORQUÉ LA NAVE AVANZA DE 10 EN 10 ENTONCES NO ERA COMPATIBLE CON 500 QUE ERA EN EL CASO DEL PROFESOR
 		POR ESO SE OPTARON POR PARAMETROS DISTINTOS EN ESTE CASO
 		*/
-		posenemigoX += (direcenemigo) ? 10: -10;
-		if(posenemigoX == 515) direcenemigo = false;
-		if(posenemigoX == 35) direcenemigo = true;
+		posenemigo1X += (direcenemigo1) ? 10: -10;
+		if(posenemigo1X == 205) direcenemigo1 = false;
+		if(posenemigo1X == 45) direcenemigo1 = true;
+		
+		posenemigo2X += (direcenemigo2) ? 10: -10;
+		if(posenemigo2X == 500) direcenemigo2 = false;
+		if(posenemigo2X == 50) direcenemigo2 = true;
+		
+		posenemigo3X += (direcenemigo3) ? 10: -10;
+		if(posenemigo3X == 500) direcenemigo3 = false;
+		if(posenemigo3X == 270) direcenemigo3 = true;
+		
+		posenemigo4X += (direcenemigo4) ? 10: -10;
+		if(posenemigo4X == 500) direcenemigo4 = false;
+		if(posenemigo4X == 50) direcenemigo4 = true;
 		
 		
 		/*
@@ -163,7 +187,10 @@ int main() {
 		espera(50);
 		borra();
 		// dibujamos el enemigo despues de cada posicion
-		enemigo(posenemigoX, posenemigoY);
+		enemigo(posenemigo1X, posenemigo1Y);
+		enemigo(posenemigo2X, posenemigo2Y);
+		enemigo(posenemigo3X, posenemigo3Y);
+		enemigo(posenemigo4X, posenemigo4Y);
 		// mostando la nave
 		nave(posnaveX, posnaveY);
 		
@@ -174,7 +201,31 @@ int main() {
 		cuando la posicion central de la nave menos 25 pixeles sea menor o igual a la posicion central del enemigo  mas 25 pixeles colapsaran
 		esto se sucere porqué estarían chocando la posicion izquiera de la nave con el lado derecho del enemigo
 		*/
-		if(posnaveX-25 <= posenemigoX+25 && posnaveX+30 >= posenemigoX-25 && posnaveY+35 >= posenemigoY-20 && posnaveY-15 <= posenemigoY+20) {
+		if(posnaveX-25 <= posenemigo1X+25 && posnaveX+30 >= posenemigo1X-25 && posnaveY+35 >= posenemigo1Y-20 && posnaveY-15 <= posenemigo1Y+20) {
+			colision(posnaveX, posnaveY);
+			refresca();
+			espera(100);
+			mensaje("GAME OVER");
+			vcierra();
+		}
+		
+		if(posnaveX-25 <= posenemigo2X+25 && posnaveX+30 >= posenemigo2X-25 && posnaveY+35 >= posenemigo2Y-20 && posnaveY-15 <= posenemigo2Y+20) {
+			colision(posnaveX, posnaveY);
+			refresca();
+			espera(100);
+			mensaje("GAME OVER");
+			vcierra();
+		}
+		
+		if(posnaveX-25 <= posenemigo3X+25 && posnaveX+30 >= posenemigo3X-25 && posnaveY+35 >= posenemigo3Y-20 && posnaveY-15 <= posenemigo3Y+20) {
+			colision(posnaveX, posnaveY);
+			refresca();
+			espera(100);
+			mensaje("GAME OVER");
+			vcierra();
+		}
+		
+		if(posnaveX-25 <= posenemigo4X+25 && posnaveX+30 >= posenemigo4X-25 && posnaveY+35 >= posenemigo4Y-20 && posnaveY-15 <= posenemigo4Y+20) {
 			colision(posnaveX, posnaveY);
 			refresca();
 			espera(100);
